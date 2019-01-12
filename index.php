48447e248ee90144c5583549539d22b4ba823787
S28667145 <43974818+S28667145@users.noreply.github.com>
2019-01-12 14:24:18 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Begin Jekyll SEO tag v2.5.0 -->
+<title>DDoS | This Is A DDoS Tool</title>
+<meta name="generator" content="Jekyll v3.7.4" />
+<meta property="og:title" content="DDoS" />
+<meta property="og:locale" content="en_US" />
+<meta name="description" content="This Is A DDoS Tool" />
+<meta property="og:description" content="This Is A DDoS Tool" />
+<link rel="canonical" href="https://s28667145.github.io/DDoS/" />
+<meta property="og:url" content="https://s28667145.github.io/DDoS/" />
+<meta property="og:site_name" content="DDoS" />
+<script type="application/ld+json">
+{"headline":"DDoS","@type":"WebSite","url":"https://s28667145.github.io/DDoS/","name":"DDoS","description":"This Is A DDoS Tool","@context":"http://schema.org"}</script>
+<!-- End Jekyll SEO tag -->
+
+    <link rel="stylesheet" href="/DDoS/assets/css/style.css?v=174ff508e1610923fa9ab9a281aadbc612a51272">
+  </head>
+  <body>
+    <div class="container-lg px-3 my-5 markdown-body">
+      
+      <h1><a href="https://s28667145.github.io/DDoS/">DDoS</a></h1>
+      
+
+      <h1><b>User Agreement</b></h1>
+<p>To download this open source script /DDoS Tool You must agree to the user agreemnet</p>
+<li>Use At your own risk it is not my fault you get arrested/or get juvinail detention</li>
+<li>Your responsiblty it is your fault if you cause damage</li>
+<p>Thats it -_-</p>
+<h2><b>Download Now</b></h2>
+<hr />
+
+<p><a href="https://github.com/S28667145/DDoS/archive/master.zip">Download DDoS Tool</a></p>
+<hr />
+
+<p><b>This Is ONLY For Windows</b></p>
+
+
+      
+      <div class="footer border-top border-gray-light mt-5 pt-3 text-right text-gray">
+        This site is open source. <a href="https://github.com/S28667145/DDoS/edit/master/README.md">Improve this page</a>.
+      </div>
+      
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/anchor-js/4.1.0/anchor.min.js" integrity="sha256-lZaRhKri35AyJSypXXs4o6OPFTbTmUoltBbDCbdzegg=" crossorigin="anonymous"></script>
+    <script>anchors.add();</script>
+    
+  </body>
+</html>
